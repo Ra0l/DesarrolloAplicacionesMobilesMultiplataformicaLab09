c27ce238c91aa25aebaafa53394f667afe18e77e
Ra0l <45242019+Ra0l@users.noreply.github.com>
2019-10-07 15:25:34 -0500

    Mi primer commit

---
+++ Ejercicio07/Ejercicio07/PickerBinding.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio07
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PickerBinding : ContentPage
+    {
+        public PickerBinding()
+        {
+            InitializeComponent();
+            BindingContext = new MonkeysPageViewModel();
+        }
+    }
+}
